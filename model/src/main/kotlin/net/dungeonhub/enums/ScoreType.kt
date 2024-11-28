e184da799f7b560f114c08e7dbdcd3c0bff18e3a
Taubsie <tobias@joerg-hartmann.de>
2024-11-28 19:24:41 +0100

    Added more localizations.

--- model/src/main/kotlin/net/dungeonhub/enums/ScoreType.kt
+++ model/src/main/kotlin/net/dungeonhub/enums/ScoreType.kt
@@ -4,24 +4,24 @@ import dev.kordex.core.commands.application.slash.converters.ChoiceEnum
 import dev.kordex.core.i18n.types.Key
 import net.dungeonhub.api.model.i18n.Translations
 import net.dungeonhub.model.carry_type.CarryTypeModel
+import java.util.Locale
 
 enum class ScoreType(
     override val readableName: Key,
-    val leaderboardSuffix: String?
+    val leaderboardSuffix: Key?
 ) : ChoiceEnum {
     Default(Translations.ScoreType.Default.readableName),
-    Alltime(Translations.ScoreType.Alltime.readableName, "(all-time)"),
-    Event(Translations.ScoreType.Event.readableName, "(event)");
+    Alltime(Translations.ScoreType.Alltime.readableName, Translations.ScoreType.Alltime.suffix),
+    Event(Translations.ScoreType.Event.readableName, Translations.ScoreType.Event.suffix);
 
     constructor(readableName: Key) : this(readableName, null)
 
-    fun getLeaderboardTitle(carryType: CarryTypeModel?): String {
-        val suffix = if (leaderboardSuffix.isNullOrBlank()) "" else " $leaderboardSuffix"
+    fun getLeaderboardTitle(carryType: CarryTypeModel?, locale: Locale? = Locale.ENGLISH): String {
+        val suffix = leaderboardSuffix?.withLocale(locale)?.translate()
 
-        if (carryType == null) {
-            return "Leaderboard | Total score$suffix"
-        }
-
-        return "Leaderboard | ${carryType.displayName}-Carries$suffix"
+        return Translations.Leaderboard.title.withLocale(locale).translateNamed(
+            "name" to (carryType?.displayName ?: Translations.Leaderboard.Title.total.withLocale(locale).translate()),
+            "suffix" to if (suffix.isNullOrBlank()) "" else " $suffix"
+        )
     }
 }
