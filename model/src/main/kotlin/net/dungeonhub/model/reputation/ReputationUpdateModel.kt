7acc56ac7f60bd48074ffa86ba0d86c8914d4a7b
Taubsie <taubsie@dungeon-hub.net>
2025-07-27 11:29:40 +0200

    Added another field that can be updated for the reputation.

--- model/src/main/kotlin/net/dungeonhub/model/reputation/ReputationUpdateModel.kt
+++ model/src/main/kotlin/net/dungeonhub/model/reputation/ReputationUpdateModel.kt
@@ -3,5 +3,6 @@ package net.dungeonhub.model.reputation
 import net.dungeonhub.structure.model.UpdateModel
 
 class ReputationUpdateModel(
-    val reason: String?
+    val reason: String?,
+    val amount: Int?
 ) : UpdateModel<ReputationModel>
