25198cd937ee5352c5fefb32915ad7770babf710
Taubsie <tobias@joerg-hartmann.de>
2026-01-01 23:50:36 +0100

    Finished implementing everything related to discord channels.

---
+++ client/src/main/kotlin/net/dungeonhub/connection/DiscordChannelConnection.kt
@@ -0,0 +1,83 @@
+package net.dungeonhub.connection
+
+import com.squareup.moshi.adapter
+import net.dungeonhub.auth.AuthenticationProvider
+import net.dungeonhub.client.AuthenticatedClient
+import net.dungeonhub.model.discord_channel.DiscordChannelCreationModel
+import net.dungeonhub.model.discord_channel.DiscordChannelModel
+import net.dungeonhub.model.discord_channel.DiscordChannelUpdateModel
+import net.dungeonhub.service.MoshiService.moshi
+import net.dungeonhub.structure.ClientlessConnection
+import net.dungeonhub.structure.Connection.Companion.jsonMediaType
+import net.dungeonhub.structure.ModuleConnection
+import okhttp3.HttpUrl
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.util.HashMap
+
+@OptIn(ExperimentalStdlibApi::class)
+class DiscordChannelConnection(private val server: Long, override val client: AuthenticatedClient) : ModuleConnection {
+    override val moduleApiPrefix = "server/$server/channel"
+
+    fun getById(id: Long): DiscordChannelModel? {
+        val url: HttpUrl = getApiUrl(id).build()
+
+        val request: Request = getApiRequest(url)
+            .get()
+            .build()
+
+        return executeRequest(request) { json: String -> DiscordChannelModel.fromJson(json) }
+    }
+
+    fun getByIdOrCreate(id: Long) : DiscordChannelModel? {
+        return getById(id) ?: addNewChannel(DiscordChannelCreationModel(id, null))
+    }
+
+    fun addNewChannel(creationModel: DiscordChannelCreationModel): DiscordChannelModel? {
+        val url: HttpUrl = getApiUrl().build()
+
+        val requestBody: RequestBody = creationModel.toJson().toRequestBody(jsonMediaType)
+
+        val request: Request = getApiRequest(url)
+            .post(requestBody)
+            .build()
+
+        return executeRequest(request) { json: String -> DiscordChannelModel.fromJson(json) }
+    }
+
+    fun updateChannel(id: Long, updateModel: DiscordChannelUpdateModel): DiscordChannelModel? {
+        val url: HttpUrl = getApiUrl(id).build()
+
+        val requestBody = updateModel.toJson().toRequestBody(jsonMediaType)
+
+        val request: Request = getApiRequest(url)
+            .put(requestBody)
+            .build()
+
+        return executeRequest(request) { json: String -> DiscordChannelModel.fromJson(json) }
+    }
+
+    val allChannels: List<DiscordChannelModel>?
+        get() {
+            val url: HttpUrl = getApiUrl("all").build()
+
+            val request: Request = getApiRequest(url).get().build()
+
+            return executeRequest(request, function = moshi.adapter<List<DiscordChannelModel>>()::fromJson)
+        }
+
+    companion object {
+        private val instances: MutableMap<Long, ClientlessDiscordChannelConnection> = HashMap()
+
+        operator fun get(server: Long): ClientlessDiscordChannelConnection {
+            return instances.computeIfAbsent(server) { ClientlessDiscordChannelConnection(it) }
+        }
+
+        class ClientlessDiscordChannelConnection(val server: Long) : ClientlessConnection<DiscordChannelConnection> {
+            override fun authenticated(authenticationProvider: AuthenticationProvider): DiscordChannelConnection {
+                return DiscordChannelConnection(server, AuthenticatedClient(authenticationProvider))
+            }
+        }
+    }
+}
--- model/src/main/kotlin/net/dungeonhub/structure/model/InitializeModel.kt
+++ model/src/main/kotlin/net/dungeonhub/structure/model/InitializeModel.kt
@@ -3,7 +3,7 @@ package net.dungeonhub.structure.model
 import net.dungeonhub.structure.entity.Entity
 import net.dungeonhub.structure.entity.InitializingFactory
 
-interface InitializeModel<E : Entity<M>, M : Model, C : CreationModel?> : Model {
+interface InitializeModel<E : Entity<M>, M : Model, C : CreationModel> : Model {
     fun toEntity(): E
 
     fun fromCreationModel(creationModel: C): InitializeModel<E, M, C>
