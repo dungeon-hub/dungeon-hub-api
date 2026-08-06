06e69c84e34414c80b3a8fd57281a24b25f7af07
Taubsie <tobias@joerg-hartmann.de>
2026-08-06 20:59:51 +0200

    Removed an unused interface.

--- model/src/main/kotlin/net/dungeonhub/structure/entity/InitializingFactory.kt
+++
@@ -1,7 +0,0 @@
-package net.dungeonhub.structure.entity
-
-import net.dungeonhub.structure.model.Model
-
-fun interface InitializingFactory<E : Entity<M>, M : Model> {
-    fun transform(entity: E): E
-}
